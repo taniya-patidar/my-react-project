745a0bf954fddcd19bbf3061a5777e6d30791525
taniya-patidar <gitgithub77@gmail.com>
2026-01-09 00:07:06 +0530

    add chang

--- code/src/App.jsx
+++ code/src/App.jsx
@@ -1,15 +1,15 @@
 import { BrowserRouter } from "react-router-dom";
-// import Routing from "./Routing"; 
+import Routing from "./Routing"; 
 import Hook from "./Hook.jsx";  
 import "./Layout.css";
 
 function App() {
   return(
-    // <BrowserRouter>
-    // {/* <Routing /> */}
+    <BrowserRouter>
+    // <Routing />
     // </BrowserRouter>
     // // <Hook/>
-    <Hook/>
+    // <Hook/>
     
   )
 }
